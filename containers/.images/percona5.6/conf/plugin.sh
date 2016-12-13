af6152722320562ac2c07e13aebc2de57ad8b64f
cucaracha <cucaracha@mail.ru>
2016-12-14 00:43:40 +0300

    InnoDB memcache SQL init

--- containers/.images/percona5.6/conf/plugin.sh
+++ containers/.images/percona5.6/conf/plugin.sh
@@ -6,7 +6,8 @@ mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "install plugin handlersocket soname '
 # MySQL InnoDB-memcached plugin
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "INSTALL PLUGIN daemon_memcached SONAME 'libmemcached.so';"
 
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "SOURCE /usr/share/mysql/innodb_memcached_config.sql;"
+# БД test может отсутствовать, используем InnoDB-Memcached.sql
+#mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "SOURCE /usr/share/mysql/innodb_memcached_config.sql;"
 
 
 
