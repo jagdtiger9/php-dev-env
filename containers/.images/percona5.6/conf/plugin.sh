3f92bc7cdc02b093ff1656ff3fcd53b30cd9082d
cucaracha <cucaracha@mail.ru>
2016-12-13 18:58:35 +0300

    add Percona container, InnoDB memcached plugin enable

--- containers/.images/percona5.6/conf/plugin.sh
+++ containers/.images/percona5.6/conf/plugin.sh
@@ -4,7 +4,7 @@
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "install plugin handlersocket soname 'handlersocket.so';"
 
 # MySQL InnoDB-memcached plugin
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "INSTALL PLUGIN daemon_memcached SONAME 'libmemcached.so';"
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "INSTALL PLUGIN daemon_memcached SONAME \"libmemcached.so\";"
 
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "SOURCE /usr/share/mysql/innodb_memcached_config.sql;"
 
