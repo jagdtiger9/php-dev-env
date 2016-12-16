aea92a7061ba76e75aafdbd5035c13298600a517
cucaracha <cucaracha@mail.ru>
2016-12-16 15:20:42 +0300

    percona: add production-like cfg my.ini

--- containers/.conf/percona/plugin.sh
+++ containers/.conf/percona/plugin.sh
@@ -6,7 +6,7 @@ mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "install plugin handlersocket soname '
 # MySQL InnoDB-memcached plugin
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "INSTALL PLUGIN daemon_memcached SONAME 'libmemcached.so';"
 
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "SOURCE /usr/share/mysql/innodb_memcached_config.sql;"
+#mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "SOURCE /usr/share/mysql/innodb_memcached_config.sql;"
 
 
 
