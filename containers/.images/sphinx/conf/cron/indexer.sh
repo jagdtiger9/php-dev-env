c4930a43d4d61a57b67f908e1fdf41cea53aed5c
cucaracha <cucaracha@mail.ru>
2016-12-21 14:45:24 +0300

    sphinx: inderxer cron log >> vardata/log/...

--- containers/.images/sphinx/conf/cron/indexer.sh
+++ containers/.images/sphinx/conf/cron/indexer.sh
@@ -4,4 +4,5 @@ echo "result=" >> /var/www/vardata/log/indexer.log
 
 /usr/bin/indexer --all --rotate --config /etc/sphinxsearch/sphinx.conf >> /var/www/vardata/log/indexer.log
 
-echo "\n\n" >> /var/www/vardata/log/indexer.log
+echo "" >> /var/www/vardata/log/indexer.log
+echo "" >> /var/www/vardata/log/indexer.log
