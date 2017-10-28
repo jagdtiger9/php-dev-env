9ce7e9008229ced7cc44f46ae4d0b85c67aa1954
cucaracha <cucaracha@mail.ru>
2017-10-28 15:49:41 +0300

    change project struct: web/public/

--- images/php/serv/.conf/cron/init_user.sh
+++ images/php/serv/.conf/cron/init_user.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-HOSTUID=$(stat -c "%u" /var/www/index.php)
-HOSTGID=$(stat -c "%g" /var/www/index.php)
+HOSTUID=$(stat -c "%u" /var/www/public/index.php)
+HOSTGID=$(stat -c "%g" /var/www/public/index.php)
 
-echo $HOSTUID $HOSTGID > /var/www/vardata/log/crontab/host_uid-gid.log
+echo $HOSTUID $HOSTGID > /var/www/public/vardata/log/crontab/host_uid-gid.log
 
 EXISTS=$(cat /etc/group | grep $HOSTGID | wc -l)
 # Create new group using target GID and add nobody user
--- images/php/serv/php5-fpm/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#exec /usr/sbin/php5-fpm --nodaemonize >>/var/log/php5-fpm.log 2>&1
-php5-fpm --nodaemonize -p /etc/php5/fpm -y /etc/php5/fpm/php-fpm.conf
