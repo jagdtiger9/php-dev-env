c584426f2dc5594f5491149b97efeb413f8f0d03
cucaracha <cucaracha@mail.ru>
2017-11-09 13:03:50 +0300

    mysql-pinba - decrease mem size

--- images/sphinx/conf/cron/indexer.sh
+++ images/sphinx/conf/cron/indexer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "result=" >> /var/www/vardata/log/indexer.log
+echo "result=" >> /var/www/public/vardata/log/indexer.log
 
-/usr/bin/indexer --all --rotate --config /etc/sphinxsearch/sphinx.conf >> /var/www/vardata/log/indexer.log
+/usr/bin/indexer --all --rotate --config /etc/sphinxsearch/sphinx.conf >> /var/www/public/vardata/log/indexer.log
 
-echo "" >> /var/www/vardata/log/indexer.log
-echo "" >> /var/www/vardata/log/indexer.log
+echo "" >> /var/www/public/vardata/log/indexer.log
+echo "" >> /var/www/public/vardata/log/indexer.log
