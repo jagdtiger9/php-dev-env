5a6dfc29e6d7b5d35a4f86b3212520ec37eb101e
cucaracha <cucaracha@mail.ru>
2019-02-01 14:45:20 +0300

    utils

--- utils/cleanup.sh
+++ utils/cleanup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Remove images + containers
-# http://blog.yohanliyanage.com/2015/05/docker-clean-up-after-yourself/
-docker rm `docker ps -aq --no-trunc --filter "status=exited"`
-docker rmi `docker images --filter 'dangling=true' -q --no-trunc`
-
-
 # Delete all containers
 docker rm $(docker ps -a -q)
 # Delete all images
 docker rmi $(docker images -q)
 
+
+# Remove images + containers
+# http://blog.yohanliyanage.com/2015/05/docker-clean-up-after-yourself/
+docker rm `docker ps -aq --no-trunc --filter "status=exited"`
+docker rmi `docker images --filter 'dangling=true' -q --no-trunc`
 
 
 # Get the latest image for container
