93a5b4d8b2c53d7287c13e5d1cab81350c0ac1ac
cucaracha <cucaracha@mail.ru>
2022-04-17 16:19:36 +0300

    add php8 jit ini params

---
+++ images/php/serv/php-fpm/start8.0.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+php-fpm8.0 --nodaemonize -p /etc/php/8.0/fpm -y /etc/php/8.0/fpm/php-fpm.conf
---
+++ images/php/serv/php-fpm/start8.1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+php-fpm8.1 --nodaemonize -p /etc/php/8.1/fpm -y /etc/php/8.1/fpm/php-fpm.conf
