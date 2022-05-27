d8ccc85adf5678c7dce8e167357bea75f8414e82
cucaracha <cucaracha@mail.ru>
2022-05-27 11:25:34 +0300

    sphinx paths

--- images/sphinx/conf/cron/indexer.sh
+++ images/sphinx/conf/cron/indexer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "result=" >> /var/www/public/vardata/log/indexer.log
+echo "result=" >> /var/www/magicpro/public/vardata/log/indexer.log
 
-/usr/bin/indexer --all --rotate --config /etc/sphinxsearch/sphinx.conf >> /var/www/public/vardata/log/indexer.log
+/usr/bin/indexer --all --rotate --config /etc/sphinxsearch/sphinx.conf >> /var/www/magicpro/public/vardata/log/indexer.log
 
-echo "" >> /var/www/public/vardata/log/indexer.log
-echo "" >> /var/www/public/vardata/log/indexer.log
+echo "" >> /var/www/magicpro/public/vardata/log/indexer.log
+echo "" >> /var/www/magicpro/public/vardata/log/indexer.log
